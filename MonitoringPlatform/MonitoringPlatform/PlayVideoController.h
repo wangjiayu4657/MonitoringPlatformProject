98fedc769e802a04fdb2cf80590a3ea2c8e67645
wangjiayu <wangjiayu@wangjiayudeMacBook-Pro.local>
2017-04-14 10:35:48 +0800

    添加PlayVideoController文件

---
+++ MonitoringPlatform/MonitoringPlatform/PlayVideoController.h
@@ -0,0 +1,13 @@
+//
+//  PlayVideoController.h
+//  MonitoringPlatform
+//
+//  Created by wangjiayu on 2017/4/14.
+//  Copyright © 2017年 wangjiayu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayVideoController : UIViewController
+
+@end
---
+++ MonitoringPlatform/MonitoringPlatform/PlayVideoController.m
@@ -0,0 +1,38 @@
+//
+//  PlayVideoController.m
+//  MonitoringPlatform
+//
+//  Created by wangjiayu on 2017/4/14.
+//  Copyright © 2017年 wangjiayu. All rights reserved.
+//
+
+#import "PlayVideoController.h"
+
+@interface PlayVideoController ()
+@property (weak, nonatomic) IBOutlet UIView *playView;
+
+@end
+
+@implementation PlayVideoController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
